3329655a9603a57ffa5d0540f80ef044e4d897f9
Cristian <me@cristian.io>
2015-03-10 23:15:48 -0700

    Allow to disable currying

--- src/ganjika/core.clj
+++ src/ganjika/core.clj
@@ -21,16 +21,22 @@
 (defn- declare-fn
   "Given a method spec create an unevaluated function form, currying
   over the provided instance"
-  [spec instance]
+  [spec instance no-currying]
   (let [method-name (symbol (:name spec))
         raw-method-name (symbol (str "." (:raw-name spec)))
         params (vec (map gensym (range (:args spec))))
         is-void (:is-void spec)]
-    `(defn ~method-name [~@params]
-       (let [return# (~raw-method-name @~instance ~@params)]
-         (if ~is-void
-           @~instance
-           return#)))))
+    (if no-currying
+      `(defn ~method-name [this# ~@params]
+         (let [return# (~raw-method-name this# ~@params)]
+           (if ~is-void
+             this#
+             return#)))
+      `(defn ~method-name [~@params]
+         (let [return# (~raw-method-name @~instance ~@params)]
+           (if ~is-void
+             @~instance
+             return#))))))
 
 (defn- define-symbol
   "Creates and return a symbol that resolves to the provided object"
@@ -39,26 +45,31 @@
         obj-symbol (gensym class-name)]
     (eval `(def ~obj-symbol ~object))))
 
-;; TODO allow to receive instance
 (defmacro def-java-methods
-  "TODO"
-  [target & {:keys [using-ns]}]
-  (let [target (eval target)
-        [clazz instance] (if (class? target)
-                           [target (.newInstance target)]
-                           [(class target) target])
+  "Maps the provided provided Java class or object methods to
+  clojure-like functions (inside using-ns if any or in the current
+  namespace). If target is a class an instance is created (assumes
+  default constructor) unless currying is disabled (via :currying
+  false)."
+  [target & {:keys [using-ns currying]}]
+  (let [no-currying (false? currying)
+        target (eval target)
+        [clazz instance] (cond
+                          (and (class? target) no-currying) [target nil]
+                          no-currying [(class target) nil]
+                          (class? target) [target (.newInstance target)]
+                          :else [(class target) target])
         specs (map method-spec (.getDeclaredMethods clazz))
-        target-sym (define-symbol instance)
+        target-sym (if-not no-currying (define-symbol instance))
         current-ns (.getName *ns*)]
-    (assert (instance? clazz instance))
+    (when-not no-currying (assert (instance? clazz instance)))
     `(do
        (when ~using-ns
          (in-ns ~using-ns))
-       ~@(map #(declare-fn % target-sym) specs)
+       ~@(map #(declare-fn % target-sym no-currying) specs)
        (in-ns (quote ~current-ns)))))
 
 (comment
-  ;; no currying
   ;; multiple arity
   (refer 'fus.noma.da :only ['mouse-move])
   (to-clj java.awt.Robot :using-ns 'fus.noma.da))
