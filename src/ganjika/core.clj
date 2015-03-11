823f353c676ce84d67e7dd6d7123e80fab260ce9
Cristian <me@cristian.io>
2015-03-11 15:49:43 -0700

    merged functions with same arity

--- src/ganjika/core.clj
+++ src/ganjika/core.clj
@@ -28,14 +28,12 @@
                :param-types (.getParameterTypes m)
                :is-static (java.lang.reflect.Modifier/isStatic (.getModifiers m))}))))
 
-(defn hint-params [spec]
-  (let [boxer {"int" "java.lang.Integer" "long" "java.lang.Long"
-               "short" "java.lang.Short" "float" "java.lang.Float"
-               "double" "java.lang.Double" "boolean" "java.lang.Boolean"
-               "byte" "java.lang.Byte" "char" "java.lang.Character"}]
-    (map #(let [name (.getName %)]
-            (str "^" (or (boxer name) name)))
-         (:param-types spec))))
+(defn hint-param [type param]
+  (let [boxed-primitive {"int" java.lang.Integer "long" java.lang.Long
+                         "short" java.lang.Short "float" java.lang.Float
+                         "double" java.lang.Double "boolean" java.lang.Boolean
+                         "byte" java.lang.Byte "char" java.lang.Character}]
+    (vary-meta param assoc :tag (or (boxed-primitive (.getName type)) type))))
 
 (defn- build-arity
   "TODO"
@@ -43,22 +41,22 @@
   (let [raw-method-name (symbol (str "." (:raw-name spec)))
         static-method-name (when (:is-static spec) (symbol (str (:class-name spec) "/" (:raw-name spec))))
         params (vec (map gensym (range (:param-count spec))))
-        hinted-params (if use-type-hinting (interleave (hint-params spec) params) params)
+        hinted-params (if use-type-hinting (map hint-param (:param-types spec) params) params)
         is-void (:is-void spec)]
     (cond
      ;; define function for static method
      (:is-static spec)
-     `([~@params] (~static-method-name ~@params))
+     `([~@hinted-params] (~static-method-name ~@params))
      ;; define non-curried function
      no-currying
-     `([this# ~@params]
+     `([this# ~@hinted-params]
        (let [result# (~raw-method-name this# ~@params)]
          (if ~is-void
            this#
            result#)))
      ;; define curried function
      :else
-     `([~@params]
+     `([~@hinted-params]
        (let [result# (~raw-method-name @~instance ~@params)]
          (if ~is-void
            @~instance
@@ -67,7 +65,7 @@
 (defn build-function
    ;; TODO overloaded arities
   "TODO make private"
-  [[fn-name specs] target-sym no-currying type-hinting]
+  [target-sym no-currying type-hinting [fn-name specs]]
   (let [fn-symbol (symbol fn-name)
         use-type-hinting type-hinting
         arity-fn (partial build-arity target-sym no-currying use-type-hinting)]
@@ -79,6 +77,16 @@
   (let [class-name (.getSimpleName (class object))
         obj-symbol (gensym class-name)]
     (eval `(def ~obj-symbol ~object))))
+
+(defn build-specs [clazz]
+  (into
+   {}
+   (for [[fn-name specs] (group-by :name (method-specs clazz))]
+     [fn-name (map (fn [[_ [spec & more-specs-with-same-arity]]]
+                     (if more-specs-with-same-arity
+                       (assoc spec :disable-hiting true)
+                       spec))
+                   (group-by :param-count specs))])))
 
 (defmacro def-java-methods
   "Maps the provided provided Java class or object methods to
@@ -95,15 +103,15 @@
                           no-currying [(class target) nil]
                           (class? target) [target (.newInstance target)]
                           :else [(class target) target])
-        specs (method-specs clazz)
-        specs-by-name (group-by :name specs)
+        specs (build-specs clazz)
         target-sym (if no-currying clazz (define-symbol instance))
-        current-ns (.getName *ns*)]
+        current-ns (.getName *ns*)
+        function-builder (partial build-function target-sym no-currying type-hinting)]
     (when-not no-currying (assert (instance? clazz instance)))
     `(do
        (when ~using-ns
          (in-ns ~using-ns))
-       ~@(map #(build-function % target-sym no-currying type-hinting) specs-by-name)
+       ~@(map function-builder specs)
        (in-ns (quote ~current-ns)))))
 
 (comment
