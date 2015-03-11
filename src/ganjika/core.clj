a0e0c28e0472f7d4b4fee94ab8167bbaf948f560
Cristian <me@cristian.io>
2015-03-11 14:08:35 -0700

    type hinting and multiple arity

--- src/ganjika/core.clj
+++ src/ganjika/core.clj
@@ -9,43 +9,69 @@
      #(str %1 (uppercase-to-hyphened-lowercase %2))
      "" name)))
 
-(defn- method-spec [m]
-  {:name (camel-to-kebab-case (.getName m))
-   :raw-name (.getName m)
-   :args (.getParameterCount m)
-   :is-void (-> m
-                 .getReturnType
-                 .getName
-                 (= "void"))
-   :is-static (java.lang.reflect.Modifier/isStatic (.getModifiers m))})
+(defn- is-public [^java.lang.reflect.Method m]
+  (java.lang.reflect.Modifier/isPublic (.getModifiers m)))
 
-(defn- declare-fn
-  "Given a method spec create an unevaluated function form, currying
-  over the provided instance"
-  [spec instance no-currying]
-  (let [method-name (symbol (:name spec))
-        raw-method-name (symbol (str "." (:raw-name spec)))
-        static-method-name (symbol (str (.getName instance) "/" (:raw-name spec)))
-        params (vec (map gensym (range (:args spec))))
+(defn- method-specs [^java.lang.Class c]
+  (->> c
+       .getDeclaredMethods
+       (filter is-public)
+       (map (fn [m]
+              {:class-name (.getName c)
+               :name (camel-to-kebab-case (.getName m))
+               :raw-name (.getName m)
+               :param-count (.getParameterCount m)
+               :is-void (-> m
+                            .getReturnType
+                            .getName
+                            (= "void"))
+               :param-types (.getParameterTypes m)
+               :is-static (java.lang.reflect.Modifier/isStatic (.getModifiers m))}))))
+
+(defn hint-params [spec]
+  (let [boxer {"int" "java.lang.Integer" "long" "java.lang.Long"
+               "short" "java.lang.Short" "float" "java.lang.Float"
+               "double" "java.lang.Double" "boolean" "java.lang.Boolean"
+               "byte" "java.lang.Byte" "char" "java.lang.Character"}]
+    (map #(let [name (.getName %)]
+            (str "^" (or (boxer name) name)))
+         (:param-types spec))))
+
+(defn- build-arity
+  "TODO"
+  [instance no-currying use-type-hinting spec]
+  (let [raw-method-name (symbol (str "." (:raw-name spec)))
+        static-method-name (when (:is-static spec) (symbol (str (:class-name spec) "/" (:raw-name spec))))
+        params (vec (map gensym (range (:param-count spec))))
+        hinted-params (if use-type-hinting (interleave (hint-params spec) params) params)
         is-void (:is-void spec)]
     (cond
      ;; define function for static method
      (:is-static spec)
-     `(def ~method-name (fn ([~@params] (~static-method-name ~@params))))
+     `([~@params] (~static-method-name ~@params))
      ;; define non-curried function
      no-currying
-     `(defn ~method-name [this# ~@params]
-        (let [return# (~raw-method-name this# ~@params)]
-          (if ~is-void
-            this#
-            return#)))
+     `([this# ~@params]
+       (let [result# (~raw-method-name this# ~@params)]
+         (if ~is-void
+           this#
+           result#)))
      ;; define curried function
      :else
-     `(defn ~method-name [~@params]
-        (let [return# (~raw-method-name @~instance ~@params)]
-          (if ~is-void
-            @~instance
-            return#))))))
+     `([~@params]
+       (let [result# (~raw-method-name @~instance ~@params)]
+         (if ~is-void
+           @~instance
+           result#))))))
+
+(defn build-function
+   ;; TODO overloaded arities
+  "TODO make private"
+  [[fn-name specs] target-sym no-currying type-hinting]
+  (let [fn-symbol (symbol fn-name)
+        use-type-hinting type-hinting
+        arity-fn (partial build-arity target-sym no-currying use-type-hinting)]
+    `(defn ~fn-symbol ~@(map arity-fn specs))))
 
 (defn- define-symbol
   "Creates and return a symbol that resolves to the provided object"
@@ -60,29 +86,28 @@
   namespace). If target is a class an instance is created (assumes
   default constructor) unless currying is disabled (via :currying
   false)."
-  [target & {:keys [using-ns currying]}]
+  [target & {:keys [using-ns currying disable-type-hinting]}]
   (let [no-currying (false? currying)
+        type-hinting (not (true? disable-type-hinting))
         target (eval target)
         [clazz instance] (cond
                           (and (class? target) no-currying) [target nil]
                           no-currying [(class target) nil]
                           (class? target) [target (.newInstance target)]
                           :else [(class target) target])
-        specs (map method-spec (.getDeclaredMethods clazz))
+        specs (method-specs clazz)
+        specs-by-name (group-by :name specs)
         target-sym (if no-currying clazz (define-symbol instance))
         current-ns (.getName *ns*)]
     (when-not no-currying (assert (instance? clazz instance)))
     `(do
        (when ~using-ns
          (in-ns ~using-ns))
-       ~@(map #(declare-fn % target-sym no-currying) specs)
+       ~@(map #(build-function % target-sym no-currying type-hinting) specs-by-name)
        (in-ns (quote ~current-ns)))))
 
 (comment
-  ;;- multiple arity
-  ;;- currying must be disabled by default for static methods
-  ;; and field/static methods with the same name??? renaming?
-  ;;- and only public methods... or make them public????
+  ;;- field/static methods with the same name??? renaming?
   ;;- better interop by coercing argument types
   (refer 'fus.noma.da :only ['mouse-move])
   (to-clj java.awt.Robot :using-ns 'fus.noma.da))
